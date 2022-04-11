5dbea5f5f5325a35edbb221a44002fa6de478398
Luis Hamilton <lhbalem@gmail.com>
2022-04-11 16:46:50 -0300

    relacionando DAO com DB por consoleApp, inserções já funcionais

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using DAO;
+
+using var context = new DaoContext();
+context.Database.EnsureCreated();
+
+context.Address.Add(new Address
+{
+    street = "Rua Alberto Rutz",
+    city = "Curitiba",
+    state = "Paraná",
+    country = "Brasil",
+    postalCode = "81320-280"
+});
+
+context.SaveChanges();
--- DAO/DaoContext.cs
+++ DAO/DaoContext.cs
@@ -1,12 +1,11 @@
 namespace DAO;
 using Microsoft.EntityFrameworkCore;
 
-public class ModelContext : DbContext
+public class DaoContext : DbContext
 {
     public DbSet<Address> Address { get; set; }
     public DbSet<Client> Client { get; set; }
     public DbSet<Owner> Owner { get; set; }
-    public DbSet<Person> Person { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<Purchase> Purchase { get; set; }
     public DbSet<Stocks> Stocks { get; set; }
@@ -15,7 +14,7 @@ public class ModelContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer("Data Source = JVLPC0571;" + "Initial Catalog = db_projetoCSharp; Integrated Security=True");
+        optionsBuilder.UseSqlServer("Data Source = JVLPC0571;" + "Initial Catalog = marketPlace; Integrated Security=True");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
